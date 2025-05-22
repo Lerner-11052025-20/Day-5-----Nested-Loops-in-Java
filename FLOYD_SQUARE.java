14b54a3dc2b1c172dba57956058b1282cf3c34b7
Deep Sorathiya <deep.professional803@gmail.com>
2025-05-22 23:54:06 +0530

    Add files via upload

---
+++ FLOYD_SQUARE.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FLOYD_SQUARE
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows : ");
+        int m = sc.nextInt();
+    
+        int a=1;
+        for(int i=1 ; i<=m ; i++)
+        {
+            for(int j=1 ; j<=m ; j++)
+            {
+                System.out.print(a+" ");
+                a++;
+            }
+            System.out.print("\n");
+        }
+    }
+}
